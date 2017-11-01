ee50f97aadc60b762baf6ed33d1c83bfe2be2d9d
Lukkascost <lucas.costa@wdatecnologia.com.br>
2017-10-31 21:20:22 -0300

    changes for export training and testing data

--- Classes/data_set.py
+++ Classes/data_set.py
@@ -6,28 +6,58 @@ class DataSet(object):
                 self.length = 0
                 self.indexSet = []
                 self.sum_confusion_matrix = None
-                self.atributes = []
-                self.labels = []
+                self.atributes = None
+                self.labels = None
                 self.number_of_samples = 0                 
         def append(self, data, traineIndexes, testIndexes):
                 if self.length == 0:
                         self.sum_confusion_matrix = np.zeros(data.confusion_matrix.shape)
                 self.sum_confusion_matrix = np.add(self.sum_confusion_matrix,data.confusion_matrix)
                 self.dataSet.append(data)
-                self.indexSet.append([traineIndexes,testIndexes])
+                if self.length == 0:
+                        self.indexSet = np.zeros(np.array([traineIndexes,testIndexes]).shape)
+                self.indexSet = np.vstack((self.indexSet, np.array([traineIndexes,testIndexes])))
+                if self.length == 0:
+                        self.indexSet = self.indexSet[1:]                
                 self.length += 1
+                
         def addSampleOfAtt(self,att):
-                self.atributes.append(att[:-1])
-                self.labels.append(att[-1])
-                self.number_of_samples += 1
-        def addSampleOfAtt(self,att,label):
-                self.atributes.append(att)
-                self.labels.append(label)
+                if self.number_of_samples == 0:
+                        self.atributes = np.zeros(att[:-1].shape)
+                        self.labels = np.zeros(att[-1].shape)                
+                self.atributes = np.vstack((self.atributes,att[:-1]))
+                self.labels = np.vstack((self.labels,att[-1]))
+                if self.number_of_samples == 0:
+                        self.atributes = self.atributes[1:]
+                        self.labels =   self.labels[1:]                       
                 self.number_of_samples += 1  
+                
+        def addSampleOfAtt(self,att,label):
+                if self.number_of_samples == 0:
+                        self.atributes = np.zeros(att.shape)
+                        self.labels = np.zeros(label.shape)                
+                self.atributes = np.vstack((self.atributes,att))
+                self.labels = np.vstack((self.labels,label))
+                if self.number_of_samples == 0:
+                        self.atributes = self.atributes[1:]
+                        self.labels =   self.labels[1:]                       
+                self.number_of_samples += 1  
+                
         def getGeneralAccurace(self):
                 generalAcc = 0
                 for i in range(self.sum_confusion_matrix.shape[0]):
                         generalAcc+= self.sum_confusion_matrix[i,i]/(self.length*self.dataSet[0].number_of_testingSamples)
                 return generalAcc/self.sum_confusion_matrix.shape[0]
+        def exportTestingSamples(self, path , index = 0):
+                atributes = self.atributes[self.indexSet[index,1]]
+                labels = self.labels[self.indexSet[index,1]] 
+                np.savetxt(path, np.hstack((atributes,labels)), delimiter=",")
+                           
+        def exportTrainingSamples(self, path , index = 0):
+                atributes = self.atributes[self.indexSet[index,0]]
+                labels = self.labels[self.indexSet[index,0]] 
+                np.savetxt(path, np.hstack((atributes,labels)), delimiter=",")
+        
+                
         def __str__(self):
                 return str(self.getGeneralAccurace())
--- testClasses.py
+++ testClasses.py
@@ -3,44 +3,51 @@ from Classes.data_set import DataSet
 from Classes.experiment import Experiment
 from Classes.Extractors.GLCM import GLCM
 
-#def ler_arquivo(address):
-        #arquivo = open(address,"r")                                     ##
-        #bd = []                                                         ##
-        #obj = 0                                                         ##
-        #for line in arquivo:                                            ##
-                #bd.append([])                                           ##
-                #lines = line.split(",")                                 ##
-                #for attr in lines:                                      ##
-                        #if(len(attr)>1): bd[obj].append(float(attr))    ##
-                #obj = obj+1                                             ##
-        #return bd                                                       ##
+def ler_arquivo(address):
+        arquivo = open(address,"r")                                     ##
+        bd = []                                                         ##
+        obj = 0                                                         ##
+        for line in arquivo:                                            ##
+                bd.append([])                                           ##
+                lines = line.split(",")                                 ##
+                for attr in lines:                                      ##
+                        if(len(attr)>1): bd[obj].append(float(attr))    ##
+                obj = obj+1                                             ##
+        return bd                                                       ##
 
 
-#experimento = Experiment()
-#for percent in [1,2]:
-        #oDataSet = DataSet()
-        #bd = ler_arquivo("Classes/Classificators/FEATURES_M{:d}.txt".format(percent))
-        #for i in bd:
-                #oDataSet.addSampleOfAtt(i[0:-1], i[-1])
-        #for it in range(50):
-                #oData = Data(7, 13)
-                #oData.confusion_matrix = 13 * np.random.rand(7, 7)
-                #a = np.arange(350)
-                #np.random.shuffle(a)
-                #t = a[:37*7]
-                #b= []
-                #for i in a:
-                        #if not i in t:
-                                #b.append(i)                    
-                #oDataSet.append(oData, t, b)
-        #experimento.addDataSet(oDataSet, description="Dataset do M={:d}:".format(percent))
-#print experimento
+experimento = Experiment()
+for percent in [1,2]:
+        oDataSet = DataSet()
+        bd = ler_arquivo("Classes/Classificators/FEATURES_M{:d}.txt".format(percent))
+        for i in bd:
+                oDataSet.addSampleOfAtt(np.array(i[0:-1]), np.array(i[-1]))
+        for it in range(50):
+                oData = Data(7, 13)
+                oData.confusion_matrix = 13 * np.random.rand(7, 7)
+                a = np.arange(350)
+                np.random.shuffle(a)
+                t = a[:37*7]
+                b= []
+                for i in a:
+                        if not i in t:
+                                b.append(i)                    
+                oDataSet.append(oData, t, b)
+                oDataSet.exportTestingSamples("testing_sample.txt")
+        experimento.addDataSet(oDataSet, description="Dataset do M={:d}:".format(percent))
+print experimento
 
 entrada = [[2,1,3,0],[0,1,1,3],[1,3,1,2],[0,1,0,2]]
-np_entrada  = np.array(entrada)
-gl = GLCM(np_entrada, 2)
 
-gl.generateCoOccurenceHorizontal()
-gl.normalizeCoOccurence()
-gl.calculateAttributes()
-print(gl.exportToClassfier(1.0))
+np_entrada  = np.array(entrada)
+
+print np_entrada
+print np.vstack((np_entrada,np_entrada[0]))
+
+
+#gl = GLCM(np_entrada, 2)
+
+#gl.generateCoOccurenceHorizontal()
+#gl.normalizeCoOccurence()
+#gl.calculateAttributes()
+#print(gl.exportToClassfier(1.0))
