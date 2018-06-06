0d0a699d063d97babe069832b3d6f40b422572df
lukkascost <lucas.costa@lit.ifce.edu.br>
2018-06-06 17:36:12 -0300

    method to export in latex table

--- Classes/data_set.py
+++ Classes/data_set.py
@@ -1,124 +1,160 @@
 from data import *
 
+
 class DataSet(object):
+    """
+            Class implemented by Lucas Costa.
+            https://www.github.com/lukkascost
+
+            Created in 30/10/2017
+            Last Modify in 19/12/2017
+
+            contact: lucas.costa@lit.ifce.edu.br
+            """
+
+    def __init__(self):
         """
-        Class implemented by Lucas Costa.
-        https://www.github.com/lukkascost
-        
-        Created in 30/10/2017
-        Last Modify in 19/12/2017
-        
-        contact: lucas.costa@lit.ifce.edu.br
-        """          
-        def __init__(self):
+                        Contructor
+                        initializes the variables with zeros or None.
+                        """
+        self.dataSet = []
+        self.length = 0
+        self.sum_confusion_matrix = None
+        self.atributes = None
+        self.labels = None
+        self.labelsNames = []
+        self.number_of_samples = 0
+        self.normalize_between = None
+
+    # ----------------------------------------------------------------------
+    def append(self, data):
+        """
+                        Parameter data: object of class type Data, with results and confusion matrix complete.
+                        adds on self object array the values of complete data object.
+                        """
+        if self.length == 0:
+            self.sum_confusion_matrix = np.zeros(data.confusion_matrix.shape)
+        self.sum_confusion_matrix = np.add(self.sum_confusion_matrix, data.confusion_matrix)
+        self.dataSet.append(data)
+        self.length += 1
+
+    # ----------------------------------------------------------------------
+    def addSampleOfAtt(self, att):
+        """
+                        Parameter att: array with attributes and label on last position.
+                        appends the attribute to object.
+                        the variable must be composed by a sequence of attributes and the last value is a label of a class which attribute belongs.
+                        """
+
+        if self.number_of_samples == 0:
+            self.atributes = np.zeros(att[:-1].shape)
+            self.labels = np.zeros(1)
+        self.atributes = np.vstack((self.atributes, att[:-1]))
+        if (att[-1] in self.labelsNames):
+            self.labels = np.vstack((self.labels, self.labelsNames.index(att[-1])))
+        else:
+            self.labelsNames.append(att[-1])
+            self.labels = np.vstack((self.labels, self.labelsNames.index(att[-1])))
+
+        if self.number_of_samples == 0:
+            self.atributes = self.atributes[1:]
+            self.labels = self.labels[1:]
+
+        self.number_of_samples += 1
+
+        return True
+
+    # ----------------------------------------------------------------------
+    def getGeneralMetrics(self):
+        """
+                        gets general metrics for results on confusion matrix, related to all the DATA append on self object.
+                        returns accurace, sensitivity and specificity.
+                        """
+        if self.length == 0: return False
+
+        metrics = np.array([i.getMetrics() for i in self.dataSet])
+        return np.average(metrics, axis=0), np.std(metrics, axis=0)
+
+    def normalizeDataSet(self):
+        """
+                        Normalize atributes in self object and saves the values of normalization max and min.
+                        """
+        self.normalize_between = np.zeros((len(self.atributes[0]), 2))
+        for i in range(len(self.atributes[0])):
+            self.normalize_between[i, 0] = max(self.atributes[:, i])
+            self.normalize_between[i, 1] = min(self.atributes[:, i])
+            if self.normalize_between[i, 0] - self.normalize_between[i, 1] == 0:
+                self.atributes[:, i] = self.atributes[:, i] * 0.0
+            else:
+                self.atributes[:, i] = self.atributes[:, i] - self.normalize_between[i, 1]
+                self.atributes[:, i] = self.atributes[:, i] / (
+                        self.normalize_between[i, 0] - self.normalize_between[i, 1])
+
+                # ----------------------------------------------------------------------
+
+    def __str__(self):
+        """
+                        """
+        string = "{:#^80}".format(" AVERAGE RESULTS ")
+        string += "\n {:^20}\t{:^6}\t{:^6}\t{:^6}\t{:^6}".format("", "acc", "Se", "Es", "F1")
+        for i in range(self.dataSet[0].number_of_classes):
+            metrics = self.getGeneralMetrics()[0]
+            string += "\n{:^20}: ".format(self.labelsNames[i])
+            string += "\t{:02.04f}".format(metrics[0][i])
+            string += "\t{:02.04f}".format(metrics[1][i])
+            string += "\t{:02.04f}".format(metrics[2][i])
+            string += "\t{:02.04f}".format(metrics[3][i])
+        string += "\n{:^20}:".format("All Class")
+        string += "\t{:02.04f}".format(metrics[0][-1])
+        string += "\t{:02.04f}".format(metrics[1][-1])
+        string += "\t{:02.04f}".format(metrics[2][-1])
+        string += "\t{:02.04f}\n".format(metrics[3][-1])
+        string += "#" * 80
+
+        string += "\n{:#^80}".format(" STD RESULTS ")
+        string += "\n {:^20}\t{:^6}\t{:^6}\t{:^6}\t{:^6}".format("", "acc", "Se", "Es", "F1")
+        for i in range(self.dataSet[0].number_of_classes):
+            metrics = self.getGeneralMetrics()[1]
+            string += "\n{:^20}: ".format(self.labelsNames[i])
+            string += "\t{:02.04f}".format(metrics[0][i])
+            string += "\t{:02.04f}".format(metrics[1][i])
+            string += "\t{:02.04f}".format(metrics[2][i])
+            string += "\t{:02.04f}".format(metrics[3][i])
+        string += "\n{:^20}:".format("All Class")
+        string += "\t{:02.04f}".format(metrics[0][-1])
+        string += "\t{:02.04f}".format(metrics[1][-1])
+        string += "\t{:02.04f}".format(metrics[2][-1])
+        string += "\t{:02.04f}\n".format(metrics[3][-1])
+        string += "#" * 80
+
+        return string
+
+    def export_as_latex_table(self, label="tab:dataset", caption="Dataset Table"):
+        """
+                :param label:
+                :param caption:
+                :return:
                 """
-                Contructor 
-                initializes the variables with zeros or None.
-                """
-                self.dataSet = []
-                self.length = 0
-                self.sum_confusion_matrix = None
-                self.atributes = None
-                self.labels = None
-                self.labelsNames = []
-                self.number_of_samples = 0    
-                self.normalize_between = None
-        #----------------------------------------------------------------------
-        def append(self, data):
-                """
-                Parameter data: object of class type Data, with results and confusion matrix complete.
-                adds on self object array the values of complete data object.                """
-                if self.length == 0:
-                        self.sum_confusion_matrix = np.zeros(data.confusion_matrix.shape)
-                self.sum_confusion_matrix = np.add(self.sum_confusion_matrix,data.confusion_matrix)
-                self.dataSet.append(data)            
-                self.length += 1
-                
-        #----------------------------------------------------------------------
-        def addSampleOfAtt(self,att):
-                """
-                Parameter att: array with attributes and label on last position.
-                appends the attribute to object.
-                the variable must be composed by a sequence of attributes and the last value is a label of a class which attribute belongs.
-                """                
-                
-                if self.number_of_samples == 0:
-                        self.atributes = np.zeros(att[:-1].shape)
-                        self.labels = np.zeros(1)                
-                self.atributes = np.vstack((self.atributes,att[:-1]))
-                if (att[-1] in  self.labelsNames):
-                        self.labels = np.vstack((self.labels,self.labelsNames.index(att[-1])))
-                else:
-                        self.labelsNames.append(att[-1])
-                        self.labels = np.vstack((self.labels,self.labelsNames.index(att[-1])))
-                        
-                if self.number_of_samples == 0:
-                        self.atributes = self.atributes[1:]
-                        self.labels =   self.labels[1:]    
-                        
-                self.number_of_samples += 1 
-                
-                return True
-        
-        #----------------------------------------------------------------------
-        def getGeneralMetrics(self):
-                """
-                gets general metrics for results on confusion matrix, related to all the DATA append on self object.
-                returns accurace, sensitivity and specificity.
-                """                
-                if self.length == 0: return False
-                
-                metrics =  np.array([i.getMetrics() for i  in self.dataSet])
-                return np.average(metrics,axis=0),np.std(metrics,axis=0)
-        
-        def normalizeDataSet(self):
-                """
-                Normalize atributes in self object and saves the values of normalization max and min.
-                """
-                self.normalize_between = np.zeros((len(self.atributes[0]),2))
-                for i in range(len(self.atributes[0])):
-                        self.normalize_between[i,0] = max(self.atributes[:,i])
-                        self.normalize_between[i,1] = min(self.atributes[:,i])              
-                        if self.normalize_between[i,0]-self.normalize_between[i,1] == 0:
-                                self.atributes[:,i] = self.atributes[:,i] * 0.0
-                        else:
-                                self.atributes[:,i] = self.atributes[:,i] - self.normalize_between[i,1]
-                                self.atributes[:,i] = self.atributes[:,i] / (self.normalize_between[i,0] - self.normalize_between[i,1])                                                    
-                                       
-        #----------------------------------------------------------------------        
-        def __str__(self):
-                """
-                """       
-                string  = "{:#^80}".format(" AVERAGE RESULTS ")
-                string += "\n {:^20}\t{:^6}\t{:^6}\t{:^6}\t{:^6}".format("","acc","Se","Es","F1")
-                for i in range(self.dataSet[0].number_of_classes):
-                        metrics = self.getGeneralMetrics()[0]
-                        string += "\n{:^20}: ".format(self.labelsNames[i])
-                        string += "\t{:02.04f}".format(metrics[0][i])
-                        string += "\t{:02.04f}".format(metrics[1][i])
-                        string += "\t{:02.04f}".format(metrics[2][i])
-                        string += "\t{:02.04f}".format(metrics[3][i])                        
-                string += "\n{:^20}:".format("All Class")
-                string += "\t{:02.04f}".format(metrics[0][-1])
-                string += "\t{:02.04f}".format(metrics[1][-1])
-                string += "\t{:02.04f}".format(metrics[2][-1])
-                string += "\t{:02.04f}\n".format(metrics[3][-1])
-                string  += "#"*80 
-                
-                string += "\n{:#^80}".format(" STD RESULTS ")
-                string += "\n {:^20}\t{:^6}\t{:^6}\t{:^6}\t{:^6}".format("","acc","Se","Es","F1")
-                for i in range(self.dataSet[0].number_of_classes):
-                        metrics = self.getGeneralMetrics()[1]
-                        string += "\n{:^20}: ".format(self.labelsNames[i])
-                        string += "\t{:02.04f}".format(metrics[0][i])
-                        string += "\t{:02.04f}".format(metrics[1][i])
-                        string += "\t{:02.04f}".format(metrics[2][i])
-                        string += "\t{:02.04f}".format(metrics[3][i])                        
-                string += "\n{:^20}:".format("All Class")
-                string += "\t{:02.04f}".format(metrics[0][-1])
-                string += "\t{:02.04f}".format(metrics[1][-1])
-                string += "\t{:02.04f}".format(metrics[2][-1])
-                string += "\t{:02.04f}\n".format(metrics[3][-1])
-                string  += "#"*80                
-                
-                return string 
+        caption = caption.replace("%", "\\%")
+        string = "\\begin{table}[htb]\n\\caption{" + caption + "}\n\\label{" + label + "}\n\\begin{" \
+                                                                                       "center}\n\\begin{" \
+                                                                                       "tabular}{@{}lccccc@{" \
+                                                                                       "}}\\hline "
+        string += "\n{:^20} & {:^6} & {:^6} & {:^6} & {:^6} \\\\\\hline \\hline".format("", "acc", "Se", "Es", "F1")
+
+        for i in range(self.dataSet[0].number_of_classes):
+            metrics = self.getGeneralMetrics()[0]
+            string += "\n{:^20} & ".format(self.labelsNames[i])
+            string += "\t{:02.04f} &".format(metrics[0][i] * 100)
+            string += "\t{:02.04f} &".format(metrics[1][i] * 100)
+            string += "\t{:02.04f} &".format(metrics[2][i] * 100)
+            string += "\t{:02.04f} \\\\\\hline".format(metrics[3][i] * 100)
+
+        string += "\n{:^20} & ".format('Totals')
+        string += "\t{:02.04f} &".format(metrics[0][-1] * 100)
+        string += "\t{:02.04f} &".format(metrics[1][-1] * 100)
+        string += "\t{:02.04f} &".format(metrics[2][-1] * 100)
+        string += "\t{:02.04f} \\\\\\hline".format(metrics[3][-1] * 100)
+
+        string += "\n\\end{tabular}\n\\end{center}\n\\end{table}\n\n"
+        return string
--- Classes/experiment.py
+++ Classes/experiment.py
@@ -4,66 +4,68 @@ import cv2
 import copy
 import gzip
 
+
 class Experiment(object):
-        """
+    """
         Class implemented by Lucas Costa.
         https://www.github.com/lukkascost
-        
+
         Created in 30/10/2017
         Last Modify in 06/02/2018
-        
+
         contact: lucas.costa@lit.ifce.edu.br
-        """        
-        def __init__(self):
+        """
+
+    def __init__(self):
+        """
+                Contructor
                 """
-                Contructor 
+        self.experimentResults = []
+        self.experimentDescript = []
+        self.length = 0
+
+    def addDataSet(self, dataSet, description=""):
+        """
+                Parameter dataSet:
+                Optional description:
                 """
-                self.experimentResults = []
-                self.experimentDescript = []
-                self.length = 0
-                
-        def addDataSet(self,dataSet, description = ""):
-                """
-                Parameter dataSet: 
-                Optional description: 
-                """
-                self.experimentResults.append(dataSet)
-                self.experimentDescript.append(description)
-                self.length+=1
-        
-        def __str__(self):
-                """
+        self.experimentResults.append(dataSet)
+        self.experimentDescript.append(description)
+        self.length += 1
+
+    def __str__(self):
+        """
                 Overwrite the str conversion to print object:
                 """
-                result = ""
-                for i,j in enumerate(self.experimentResults):
-                        result += "*"*40+self.experimentDescript[i] + "*"*40 + "\n"
-                        result += str(j) + "\n"
+        result = ""
+        for i, j in enumerate(self.experimentResults):
+            result += "*" * 40 + self.experimentDescript[i] + "*" * 40 + "\n"
+            result += str(j) + "\n"
 
-                return result
-        
-        def save(self, filename, protocol = 0):
-                """
+        return result
+
+    def save(self, filename, protocol=0):
+        """
                 Saves a compressed object to disk
-                Parameter filename: 
-                Optional Parameter: 
+                Parameter filename:
+                Optional Parameter:
                 """
-                fileRes = gzip.GzipFile(filename, 'wb')
-                fileRes.write(pk.dumps(self, protocol))
-                fileRes.close()        
-                
-        def load(self,filename):
-                """
+        fileRes = gzip.GzipFile(filename, 'wb')
+        fileRes.write(pk.dumps(self, protocol))
+        fileRes.close()
+
+    def load(self, filename):
+        """
                 Loads a compressed object from disk
                 Parameter filename:
                 """
-                fileRes = gzip.GzipFile(filename, 'rb')
-                ClassFile = ""
-                while True:
-                        data = fileRes.read()
-                        if data == "":
-                                break
-                        ClassFile += data
-                result = pk.loads(ClassFile)
-                fileRes.close()
-                return result
+        fileRes = gzip.GzipFile(filename, 'rb')
+        ClassFile = ""
+        while True:
+            data = fileRes.read()
+            if data == "":
+                break
+            ClassFile += data
+        result = pk.loads(ClassFile)
+        fileRes.close()
+        return result
