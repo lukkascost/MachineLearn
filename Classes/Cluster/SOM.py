e0dac87c52ad8ba451a63b74386206b0795040f1
lukkascost <lucas.costa@lit.ifce.edu.br>
2019-05-03 13:53:43 -0300

    initial implementation of SOM

--- Classes/Cluster/SOM.py
+++ Classes/Cluster/SOM.py
@@ -1,3 +1,47 @@
+import numpy as np
+
 class SOM:
-    def __init__(self):
-        pass
+    def __init__(self, epoch, n_neurons, attributes_dimension):
+        self.epochs = epoch
+        self.n_neurons = n_neurons
+        self.att_dimension = attributes_dimension
+        self.neurons_matrix = np.zeros((n_neurons, n_neurons, attributes_dimension))
+        self.hist_neurons_matrix = np.zeros((self.epochs, n_neurons, n_neurons, attributes_dimension))
+
+    def cluster_data(self, data, init_radius, i_learning_rate):
+        time_constant = len(data) / np.log(init_radius)
+        for ep in range(self.epochs):
+            for itera, att in enumerate(data):
+                winner = get_winner(self.neurons_matrix, att)
+                radius = decay_radius(init_radius, itera, time_constant)
+                learning_rate = decay_learning_rate(i_learning_rate, itera, len(data))
+
+                for i in range(self.neurons_matrix.shape[0]):
+                    for j in range(self.neurons_matrix.shape[1]):
+                        w_dist = np.sum((np.array([i, j]) - winner) ** 2)
+                        w_dist = np.sqrt(w_dist)
+                        if w_dist <= radius:
+                            influence = calculate_influence(w_dist, radius)
+                            self.neurons_matrix[i, j] = self.neurons_matrix[i, j] + learning_rate * influence * (
+                                    att - self.neurons_matrix[i, j])
+            self.hist_neurons_matrix[ep] = self.neurons_matrix
+
+def get_winner(el1, el2):
+    d = distance(el1, el2)
+    best_element = np.argmin(d)
+    return np.array([best_element // el1.shape[0], best_element % el1.shape[1]])
+
+def distance(el1, el2):
+    return np.sqrt(np.sum((el2 - el1) ** 2, axis=-1))
+
+
+def calculate_influence(distance, radius):
+    return np.exp(-distance / (2 * (radius ** 2)))
+
+
+def decay_radius(initial_radius, i, time_constant):
+    return initial_radius * np.exp(-i / time_constant)
+
+
+def decay_learning_rate(initial_learning_rate, i, n_iterations):
+    return initial_learning_rate * np.exp(-i / n_iterations)
--- SomImpl.py
+++ SomImpl.py
@@ -9,36 +9,18 @@ import numpy as np
 
 import matplotlib.pyplot as plt
 
+from Classes.Cluster.SOM import SOM, distance
+
 COLOR = cm.rainbow(np.linspace(0, 1, 8))
 
-# mu, sigma = 1, 0.4
-# mu2, sigma2 = -1, 0.4
-# qtd = 500
-
 n_neurons = 40
-i_learning_rate = 0.1
-epochs = 20
+i_learning_rate = 0.01
+epochs = 2
 n_class = 7
 
 k_nn = 5
 
 use_train = 0.8
-
-# s = np.random.normal(mu, sigma, qtd)
-# s2 = np.random.normal(mu, sigma, qtd)
-# c1 = np.column_stack((s, s2, np.ones(qtd) * 0))
-#
-# s3 = np.random.normal(mu2, sigma2, qtd)
-# s4 = np.random.normal(mu, sigma2, qtd)
-# c2 = np.column_stack((s3, s4, np.ones(qtd) * 1))
-#
-# s5 = np.random.normal(mu, sigma2, qtd)
-# s6 = np.random.normal(mu2, sigma2, qtd)
-# c3 = np.column_stack((s5, s6, np.ones(qtd) * 2))
-#
-# s7 = np.random.normal(mu2, sigma2, qtd)
-# s8 = np.random.normal(mu2, sigma2, qtd)
-# c4 = np.column_stack((s7, s8, np.ones(qtd) * 3))
 
 att = np.loadtxt("../Fluxo_experiments/GLCM/EXP_01/FEATURES_M1_CM8b.txt", usecols=[x for x in range(24)], delimiter=",")
 label = np.loadtxt("../Fluxo_experiments/GLCM/EXP_01/FEATURES_M1_CM8b.txt", usecols=-1, dtype=object, delimiter=",")
@@ -58,67 +40,11 @@ time_constant = iterations / np.log(init_radius)
 
 oData = Data(n_class, 11, samples=47)
 oData.random_training_test_by_percent([352, 382, 378, 382, 376, 360, 361], use_train)
-print(oData.Testing_indexes.shape)
-print(oData.Training_indexes)
 
+som = SOM(epochs, n_neurons, oDataSet.attributes.shape[1])
+som.cluster_data(oDataSet.attributes[oData.Training_indexes], init_radius, i_learning_rate)
 
-def get_winner(el1, el2):
-    d = distance(el1, el2)
-    best_element = np.argmin(d)
-    return np.array([best_element // el1.shape[0], best_element % el1.shape[1]])
-
-
-def distance(el1, el2):
-    return np.sqrt(np.sum((el2 - el1) ** 2, axis=-1))
-
-
-def calculate_influence(distance, radius):
-    return np.exp(-distance / (2 * (radius ** 2)))
-
-
-def decay_radius(initial_radius, i, time_constant):
-    return initial_radius * np.exp(-i / time_constant)
-
-
-def decay_learning_rate(initial_learning_rate, i, n_iterations):
-    return initial_learning_rate * np.exp(-i / n_iterations)
-
-
-neurons_matrix = np.zeros((n_neurons, n_neurons, oDataSet.attributes.shape[1]))
-print(neurons_matrix.shape)
-for ep in range(epochs):
-    np.random.shuffle(oData.Training_indexes)
-    print(ep)
-    for itera, index in enumerate(oData.Training_indexes):
-        winner = get_winner(neurons_matrix, oDataSet.attributes[index])
-        neighboards = decay_radius(init_radius, itera, time_constant)
-        learning_rate = decay_learning_rate(i_learning_rate, itera, iterations)
-
-        for i in range(neurons_matrix.shape[0]):
-            for j in range(neurons_matrix.shape[1]):
-                w_dist = np.sum((np.array([i, j]) - winner) ** 2)
-                w_dist = np.sqrt(w_dist)
-                if w_dist <= neighboards:
-                    influence = calculate_influence(w_dist, neighboards)
-                    neurons_matrix[i, j] = neurons_matrix[i, j] + learning_rate * influence * (
-                            oDataSet.attributes[index] - neurons_matrix[i, j])
-                    # print(neighboards, influence, learning_rate, winner, [i,j])
-
-# plt.scatter(s, s2, color=COLOR[0])
-# plt.scatter(s3, s4, color=COLOR[1])
-# plt.scatter(s5, s6, color=COLOR[2])
-# plt.scatter(s7, s8, color=COLOR[3])
-# plt.scatter(neurons_matrix[:, :, 0].reshape(n_neurons * n_neurons),
-#             neurons_matrix[:, :, 1].reshape(n_neurons * n_neurons), linewidths=2, color=COLOR[4])
-#
-# for i in range(n_neurons * n_neurons):
-#     plt.annotate(str(i // n_neurons) + "," + str(i % n_neurons), (
-#         neurons_matrix[i // n_neurons, i % n_neurons, 0], neurons_matrix[i // n_neurons, i % n_neurons, 1]))
-# plt.xlim([-2.5, 2.5])
-# plt.ylim([-2.5, 2.5])
-#
-# plt.show()
-
+for ep,neurons_matrix in enumerate(som.hist_neurons_matrix):
     labels = np.zeros(neurons_matrix.shape[:2])
     for i in range(neurons_matrix.shape[0]):
         for j in range(neurons_matrix.shape[1]):
@@ -142,7 +68,7 @@ for ep in range(epochs):
             plt.scatter(x, y, linewidths=1, color=COLOR[int(labels[x, y])])
 
     plt.legend(loc='upper left', bbox_to_anchor=(1.04, 1))
-    plt.savefig("FIG_5/Test_{:3d}.png".format(ep), dpi=300, bbox_inches="tight")
+    plt.savefig("Test_{:3d}.png".format(ep), dpi=300, bbox_inches="tight")
     plt.show()
 
 
@@ -157,6 +83,6 @@ for ep in range(epochs):
             plt.scatter(neurons_matrix[x,y,0], neurons_matrix[x,y,1], linewidths=1, color=COLOR[int(labels[x, y])])
 
     plt.legend(loc='upper left', bbox_to_anchor=(1.04, 1))
-    plt.savefig("FIG_6/U_{:3d}.png".format(ep), dpi=300, bbox_inches="tight")
+    plt.savefig("U_{:3d}.png".format(ep), dpi=300, bbox_inches="tight")
     plt.show()
 
