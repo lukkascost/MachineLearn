274b350f1901c29b811cdcf22f76af12d75ce66c
lukkascost <lucas.costa@lit.ifce.edu.br>
2019-05-05 15:07:13 -0300

    som implemented

--- Classes/Cluster/SOM.py
+++ Classes/Cluster/SOM.py
@@ -1,4 +1,6 @@
 import numpy as np
+import gzip
+import pickle as pk
 
 class SOM:
     def __init__(self, epoch, n_neurons, attributes_dimension):
@@ -6,17 +8,22 @@ class SOM:
         self.n_neurons = n_neurons
         self.att_dimension = attributes_dimension
         self.neurons_matrix = np.zeros((n_neurons, n_neurons, attributes_dimension))
-        self.hist_neurons_matrix = np.zeros((self.epochs, n_neurons, n_neurons, attributes_dimension))
+        self.hist_neurons_matrix = []
+        self.hist_error = []
 
     def cluster_data(self, data, init_radius, i_learning_rate):
-        time_constant = len(data) / np.log(init_radius)
-        for ep in range(self.epochs):
+        time_constant = 0.0001
+        time_constant2 = 0.00001
+
+        print("Iniciando Treinamento...")
+        continues = True
+        ep = 0
+        while continues:
+            print("\n\n\n\nIniciando Epoca ", ep)
             for itera, att in enumerate(data):
                 winner = get_winner(self.neurons_matrix, att)
-                radius = decay_radius(init_radius, itera, time_constant)
-                learning_rate = decay_learning_rate(i_learning_rate, itera, len(data))
-                print(radius, learning_rate)
-
+                radius = decay_radius(init_radius, itera + (len(data) * ep), time_constant)
+                learning_rate = decay_learning_rate(i_learning_rate, itera + (len(data) * ep), time_constant2)
                 for i in range(self.neurons_matrix.shape[0]):
                     for j in range(self.neurons_matrix.shape[1]):
                         w_dist = np.sum((np.array([i, j]) - winner) ** 2)
@@ -25,12 +32,62 @@ class SOM:
                             influence = calculate_influence(w_dist, radius)
                             self.neurons_matrix[i, j] = self.neurons_matrix[i, j] + learning_rate * influence * (
                                     att - self.neurons_matrix[i, j])
-            self.hist_neurons_matrix[ep] = self.neurons_matrix
+
+            self.hist_neurons_matrix.append(self.neurons_matrix)
+            print(radius, learning_rate)
+            print("Epoca ", ep, "encerrada")
+            print("Iniciando calculo de erro de quantizacao...")
+            self.calculate_error(ep, data)
+            if ep == 0:
+                ep += 1
+                continue
+            print("Condicao de parada",abs(self.hist_error[ep - 1] - self.hist_error[ep]))
+            if abs(self.hist_error[ep - 1] - self.hist_error[ep]) < 0.0005 or ep >30:
+                continues = False
+            ep += 1
+
+    def calculate_error(self, ep, data):
+        self.hist_error.append(0)
+        for itera, att in enumerate(data):
+            winner = get_winner(self.neurons_matrix, att)
+            self.hist_error[ep] += np.sum((att - self.hist_neurons_matrix[ep][winner[0], winner[1]]) ** 2)
+        self.hist_error[ep] /= data.shape[0]
+        print("Erro de quantizacao encontrado.")
+        print(self.hist_error[ep])
+
+    def save(self, filename, protocol=0):
+        """
+        Saves a compressed object to disk
+        Parameter filename:
+        Optional Parameter:
+        """
+        file_res = gzip.GzipFile(filename, 'wb')
+        file_res.write(pk.dumps(self, protocol))
+        file_res.close()
+
+    @staticmethod
+    def load(filename):
+        """
+        Loads a compressed object from disk
+        Parameter filename:
+        """
+        file_res = gzip.GzipFile(filename, 'rb')
+        class_file = b''
+        while True:
+            data = file_res.read()
+            if data == b'':
+                break
+            class_file += data
+        result = pk.loads(class_file)
+        file_res.close()
+        return result
+
 
 def get_winner(el1, el2):
     d = distance(el1, el2)
     best_element = np.argmin(d)
     return np.array([best_element // el1.shape[0], best_element % el1.shape[1]])
+
 
 def distance(el1, el2):
     return np.sqrt(np.sum((el2 - el1) ** 2, axis=-1))
@@ -41,8 +98,8 @@ def calculate_influence(distance, radius):
 
 
 def decay_radius(initial_radius, i, time_constant):
-    return initial_radius * np.exp(-i / time_constant)
+    return initial_radius * np.exp(-i * time_constant)
 
 
 def decay_learning_rate(initial_learning_rate, i, n_iterations):
-    return initial_learning_rate * np.exp(-i / n_iterations)
+    return initial_learning_rate * np.exp(-i * n_iterations)
--- SomImpl.py
+++ SomImpl.py
@@ -13,9 +13,9 @@ from Classes.Cluster.SOM import SOM, distance
 
 COLOR = cm.rainbow(np.linspace(0, 1, 8))
 
-n_neurons = 40
-i_learning_rate = 0.1
-epochs = 2
+n_neurons = 20
+i_learning_rate = 0.3
+epochs = 1
 n_class = 7
 
 k_nn = 5
@@ -28,34 +28,37 @@ label = np.loadtxt("../Fluxo_experiments/GLCM/EXP_01/FEATURES_M1_CM8b.txt", usec
 # initial neighbourhood radius
 init_radius = np.max([n_neurons, n_neurons])
 # radius decay parameter
-
+exp = Experiment()
 oDataSet = DataSet()
 for j, i in enumerate(att):
     oDataSet.add_sample_of_attribute(np.array(list(i) + [label[j]]))
 oDataSet.attributes = oDataSet.attributes.astype(float)
 # oDataSet.normalize_data_set()
-# oDataSet.attributes = np.nan_to_num((oDataSet.attributes - np.mean(oDataSet.attributes, axis=0))/np.std(oDataSet.attributes, axis=0))
+
+oDataSet.attributes = np.nan_to_num(
+    (oDataSet.attributes - np.mean(oDataSet.attributes, axis=0)) / np.std(oDataSet.attributes, axis=0))
 iterations = (len(oDataSet.attributes) * use_train)
 time_constant = iterations / np.log(init_radius)
+for n in range(50):
+    oData = Data(n_class, 11, samples=47)
+    oData.random_training_test_by_percent([352, 382, 378, 382, 376, 360, 361], use_train)
 
-oData = Data(n_class, 11, samples=47)
-oData.random_training_test_by_percent([352, 382, 378, 382, 376, 360, 361], use_train)
+    som = SOM(epochs, n_neurons, oDataSet.attributes.shape[1])
+    som.cluster_data(oDataSet.attributes[oData.Training_indexes], init_radius, i_learning_rate)
 
-som = SOM(epochs, n_neurons, oDataSet.attributes.shape[1])
-som.cluster_data(oDataSet.attributes[oData.Training_indexes], init_radius, i_learning_rate)
-
-for ep,neurons_matrix in enumerate(som.hist_neurons_matrix):
-    labels = np.zeros(neurons_matrix.shape[:2])
-    for i in range(neurons_matrix.shape[0]):
-        for j in range(neurons_matrix.shape[1]):
+    som.save("EXP_05.gzip")
+    labels = np.zeros(som.neurons_matrix.shape[:2])
+    for i in range(som.neurons_matrix.shape[0]):
+        for j in range(som.neurons_matrix.shape[1]):
             labels[i, j] = 0
             count_label = np.zeros((n_class, 1))
-            vizinhos = distance(oDataSet.attributes, neurons_matrix[i, j])
+            vizinhos = distance(oDataSet.attributes, som.neurons_matrix[i, j])
             for k in range(k_nn):
                 min = np.argmin(vizinhos)
                 count_label[int(oDataSet.labels[min, 0])] += 1
                 vizinhos[min] = np.max(vizinhos)
             labels[i, j] = np.argmax(count_label)
+
     ok = np.zeros((n_class, 1))
     for i in range(n_neurons ** 2):
         x = i // n_neurons
@@ -66,23 +69,9 @@ for ep,neurons_matrix in enumerate(som.hist_neurons_matrix):
             ok[int(labels[x, y])] = 1
         else:
             plt.scatter(x, y, linewidths=1, color=COLOR[int(labels[x, y])])
-
+    oData.model = som
     plt.legend(loc='upper left', bbox_to_anchor=(1.04, 1))
-    plt.savefig("Test_{:3d}.png".format(ep), dpi=300, bbox_inches="tight")
     plt.show()
-
-
-    for i in range(n_neurons ** 2):
-        x = i // n_neurons
-        y = i % n_neurons
-        if ok[int(labels[x, y])] == 0:
-            plt.scatter(neurons_matrix[x,y,0], neurons_matrix[x,y,1], linewidths=1, color=COLOR[int(labels[x, y])],
-                        label="{}".format(oDataSet.labelsNames[int(labels[x, y])]))
-            ok[int(labels[x, y])] = 1
-        else:
-            plt.scatter(neurons_matrix[x,y,0], neurons_matrix[x,y,1], linewidths=1, color=COLOR[int(labels[x, y])])
-
-    plt.legend(loc='upper left', bbox_to_anchor=(1.04, 1))
-    plt.savefig("U_{:3d}.png".format(ep), dpi=300, bbox_inches="tight")
-    plt.show()
-
+    oDataSet.append(oData)
+exp.add_data_set(oDataSet, "{} neuronios e learning de {}".format(n_neurons, i_learning_rate))
+exp.save("Experiment_01")
