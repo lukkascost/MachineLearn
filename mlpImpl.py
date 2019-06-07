7d151bd8fb6a4ecb5744c71b3e14eefd84db1216
lukkascost <lucas.costa@lit.ifce.edu.br>
2019-06-06 21:24:28 -0300

    Results for experiment 02 and new test to run.

--- mlpImpl.py
+++ mlpImpl.py
@@ -6,13 +6,13 @@ import warnings
 from Classes import DataSet, Data, Experiment
 from Classes.Classificators.multilayer_perceptron import MLPClassifier_lucas
 import matplotlib.pyplot as plt
-init = 0.5
+init = 0.01
 
 X = np.loadtxt("../Fluxo_experiments/GLCM/EXP_01/FEATURES_M1_CM8b.txt", delimiter=',', usecols=[x for x in range(24)])
 Y = np.loadtxt("../Fluxo_experiments/GLCM/EXP_01/FEATURES_M1_CM8b.txt", delimiter=',', usecols=-1, dtype=object)
 
 oExp = Experiment()
-for learn in range(0, 51, 1):
+for learn in range(0, 10, 1):
     oDataSet = DataSet()
 
     for j, i in enumerate(X):
@@ -23,7 +23,7 @@ for learn in range(0, 51, 1):
         (oDataSet.attributes - np.mean(oDataSet.attributes, axis=0)) / np.std(oDataSet.attributes, axis=0))
 
     for iteration in range(30):
-        print(learn ,init - (learn*0.01), iteration)
+        print(learn ,init - (learn*0.001), iteration)
         oData = Data(7, 10)
 
         oData.random_training_test_by_percent([352, 382, 378, 382, 376, 360, 361], 0.8)
@@ -33,7 +33,7 @@ for learn in range(0, 51, 1):
                                   alpha=0.0001,
                                   batch_size=200,
                                   learning_rate="constant",
-                                  learning_rate_init=init - (learn*0.01),
+                                  learning_rate_init=init - (learn*0.001),
                                   verbose=False,
                                   power_t=0.5,
                                   max_iter=20000,
@@ -54,9 +54,9 @@ for learn in range(0, 51, 1):
         oData.params = clf.get_params()
         oData.model = clf
         oDataSet.append(oData)
-    oExp.add_data_set(oDataSet, "Experimento variando taxa de aprendixado {} .".format(init - (learn*0.01)))
-    oExp.save("Experiment_02_01_MLP_LEARN_BK.gzip")
-    oExp.save("Experiment_02_01_MLP_LEARN.gzip")
+    oExp.add_data_set(oDataSet, "Experimento variando taxa de aprendixado {} .".format(init - (learn*0.001)))
+    oExp.save("Experiment_02_02_MLP_LEARN_BK.gzip")
+    oExp.save("Experiment_02_02_MLP_LEARN.gzip")
     print(oExp)
 x = []
 y = []
@@ -64,26 +64,16 @@ y_p = []
 y_m = []
 METRIC = 1
 
-oExp = Experiment.load("Experiment_01_01_MLP_NEURON.gzip")
+oExp = Experiment.load("Experiment_02_01_MLP_LEARN.gzip")
 for i in oExp.experimentResults:
-    x.append(i.dataSet[0].model.hidden_layer_sizes[0])
+    x.append(i.dataSet[0].model.learning_rate_init)
+    for j in i.dataSet:
+        y_pred = j.model.predict(i.attributes[j.Training_indexes])
+        j.confusion_matrix = confusion_matrix(i.labels[j.Training_indexes], y_pred)
     y.append(i.get_general_metrics()[0][METRIC, -1])
     y_p.append(i.get_general_metrics()[0][METRIC, -1] + i.get_general_metrics()[1][METRIC, -1])
     y_m.append(i.get_general_metrics()[0][METRIC, -1] - i.get_general_metrics()[1][METRIC, -1])
 
-oExp = Experiment.load("Experiment_01_02_MLP_NEURON.gzip")
-for i in oExp.experimentResults:
-    x.append(i.dataSet[0].model.hidden_layer_sizes[0])
-    y.append(i.get_general_metrics()[0][METRIC, -1])
-    y_p.append(i.get_general_metrics()[0][METRIC, -1] + i.get_general_metrics()[1][METRIC, -1])
-    y_m.append(i.get_general_metrics()[0][METRIC, -1] - i.get_general_metrics()[1][METRIC, -1])
-
-oExp = Experiment.load("Experiment_01_03_MLP_NEURON.gzip")
-for i in oExp.experimentResults:
-    x.append(i.dataSet[0].model.hidden_layer_sizes[0])
-    y.append(i.get_general_metrics()[0][METRIC, -1])
-    y_p.append(i.get_general_metrics()[0][METRIC, -1] + i.get_general_metrics()[1][METRIC, -1])
-    y_m.append(i.get_general_metrics()[0][METRIC, -1] - i.get_general_metrics()[1][METRIC, -1])
 print(x[-20:-15])
 print(y_p[-20:-15])
 print(y[-20:-15])
@@ -91,5 +81,5 @@ print(y[-20:-15])
 plt.plot(x, y)
 plt.scatter(x, y_p)
 plt.scatter(x, y_m)
-plt.savefig("FIG1/SE_x_NEURONS.png".format(), dpi=300, bbox_inches="tight")
+plt.savefig("FIG1/SE_x_LEARN_TRAIN.png".format(), dpi=300, bbox_inches="tight")
 plt.show()
