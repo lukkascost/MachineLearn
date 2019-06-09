d00f7a94a3a8dd01560fe32541d05305e22effa0
lukkascost <lucas.costa@lit.ifce.edu.br>
2019-06-09 12:57:12 -0300

    Resultado com 10 atributos

--- mlpImpl.py
+++ mlpImpl.py
@@ -8,7 +8,10 @@ from Classes.Classificators.multilayer_perceptron import MLPClassifier_lucas
 import matplotlib.pyplot as plt
 init = 0.01
 
-X = np.loadtxt("../Fluxo_experiments/GLCM/EXP_01/FEATURES_M1_CM8b.txt", delimiter=',', usecols=[x for x in range(24)])
+basemask = np.array([1, 2, 5, 9, 15, 16, 17, 21, 22, 23])
+basemask = basemask - 1
+
+X = np.loadtxt("../Fluxo_experiments/GLCM/EXP_01/FEATURES_M1_CM8b.txt", delimiter=',', usecols=basemask)
 Y = np.loadtxt("../Fluxo_experiments/GLCM/EXP_01/FEATURES_M1_CM8b.txt", delimiter=',', usecols=-1, dtype=object)
 
 oExp = Experiment()
@@ -22,7 +25,7 @@ for learn in range(0, 1, 1):
     oDataSet.attributes = np.nan_to_num(
         (oDataSet.attributes - np.mean(oDataSet.attributes, axis=0)) / np.std(oDataSet.attributes, axis=0))
 
-    for iteration in range(1):
+    for iteration in range(30):
         print(learn ,init - (learn*0.001), iteration)
         oData = Data(7, 10)
 
@@ -34,7 +37,7 @@ for learn in range(0, 1, 1):
                                   batch_size=200,
                                   learning_rate="constant",
                                   learning_rate_init=0.05,
-                                  verbose=False,
+                                  verbose=True,
                                   power_t=0.5,
                                   max_iter=20000,
                                   shuffle=True,
@@ -49,17 +52,17 @@ for learn in range(0, 1, 1):
                 print()
                 break
             y_pred, _y = clf.predict(oDataSet.attributes[oData.Testing_indexes])
-            plt.clf()
-            plt.scatter([x for x in range(len(oDataSet.attributes[oData.Testing_indexes]))],_y[:,6])
-            plt.scatter([x for x in range(len(oDataSet.attributes[oData.Testing_indexes]))],_y[:,0])
-            plt.savefig("FIG2/{:04d}.png".format(k), dpi=201, bbox_inches="tight")
+            # plt.clf()
+            # plt.scatter([x for x in range(len(oDataSet.attributes[oData.Testing_indexes]))],_y[:,6])
+            # plt.scatter([x for x in range(len(oDataSet.attributes[oData.Testing_indexes]))],_y[:,0])
+            # plt.savefig("FIG2/{:04d}.png".format(k), dpi=201, bbox_inches="tight")
         oData.confusion_matrix = confusion_matrix(oDataSet.labels[oData.Testing_indexes], y_pred)
         oData.params = clf.get_params()
         oData.model = clf
         oDataSet.append(oData)
-    oExp.add_data_set(oDataSet, "Experimento variando taxa de aprendixado {} .".format(init - (learn*0.001)))
-    oExp.save("Experiment_02_02_MLP_LEARN_BK.gzip")
-    oExp.save("Experiment_02_02_MLP_LEARN.gzip")
+    oExp.add_data_set(oDataSet, "Experimento variando Com 10 atributos .".format(init - (learn*0.001)))
+    oExp.save("Experiment_03_01_MLP_BK.gzip")
+    oExp.save("Experiment_03_01_MLP.gzip")
     print(oExp)
 x = []
 y = []
@@ -67,31 +70,10 @@ y_p = []
 y_m = []
 METRIC = 0
 
-# oExp = Experiment.load("Experiment_02_01_MLP_LEARN.gzip")
-# for i in oExp.experimentResults:
-#     x.append(i.dataSet[0].model.learning_rate_init)
-#     # for j in i.dataSet:
-#     #     y_pred = j.model.predict(i.attributes[j.Training_indexes])
-#     #     j.confusion_matrix = confusion_matrix(i.labels[j.Training_indexes], y_pred)
-#     y.append(i.get_general_metrics()[0][METRIC, -1])
-#     y_p.append(i.get_general_metrics()[0][METRIC, -1] + i.get_general_metrics()[1][METRIC, -1])
-#     y_m.append(i.get_general_metrics()[0][METRIC, -1] - i.get_general_metrics()[1][METRIC, -1])
-oExp = Experiment.load("Experiment_02_02_MLP_LEARN.gzip")
-for i in oExp.experimentResults:
-    x.append(i.dataSet[0].model.learning_rate_init)
-    # for j in i.dataSet:
-    #     y_pred = j.model.predict(i.attributes[j.Training_indexes])
-    #     j.confusion_matrix = confusion_matrix(i.labels[j.Training_indexes], y_pred)
-    y.append(i.get_general_metrics()[0][METRIC, -1])
-    y_p.append(i.get_general_metrics()[0][METRIC, -1] + i.get_general_metrics()[1][METRIC, -1])
-    y_m.append(i.get_general_metrics()[0][METRIC, -1] - i.get_general_metrics()[1][METRIC, -1])
+oExp = Experiment.load("Experiment_03_01_MLP.gzip")
 
-print(x[-20:-15])
-print(y_p[-20:-15])
-print(y[-20:-15])
+oDataSet = oExp.experimentResults[0]
 
-plt.plot(x, y)
-plt.scatter(x, y_p)
-plt.scatter(x, y_m)
-plt.savefig("FIG1/ACC_x_LEARN_2.png".format(), dpi=300, bbox_inches="tight")
-plt.show()
+print(oDataSet)
+print(oDataSet.sum_confusion_matrix/30)
+
