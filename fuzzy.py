3733883309a9562c365641257c317183d26f0ec2
lukkascost <lucas.costa@lit.ifce.edu.br>
2019-06-23 15:15:33 -0300

    initial fuzzy impl

---
+++ fuzzy.py
@@ -0,0 +1,58 @@
+import numpy as np
+
+
+def calc_y_desc(data, coefs):
+    a = 1 / (-coefs[0] + coefs[1])
+    b = 1 + (coefs[0] / (-coefs[0] + coefs[1]))
+    return (-a) * data + b
+
+
+def calc_y_asc(data, coefs):
+    a = 1 / (-coefs[0] + coefs[1])
+    b = (-coefs[0] / (-coefs[0] + coefs[1]))
+    return (a) * data + b
+
+
+data = np.loadtxt("iris.data", usecols=[0, 1, 2, 3], delimiter=",")
+labels = np.loadtxt("iris.data", usecols=-1, delimiter=",", dtype=object)
+data_fuzzy = np.zeros(data.shape, dtype=object)
+data_pert = np.zeros((150, 4, 5))
+NOMES = ['MUITO PEQUENO', 'PEQUENO','MEDIO','GRANDE','MUITO GRANDE']
+
+coefs_all = [[4.3, 5.02, 5.74, 6.46, 7.18, 7.9],
+             [2.0, 2.48, 2.96, 3.44, 3.92, 4.4],
+             [1.0, 2.18, 3.3600000000000003, 4.540000000000001, 5.720000000000001, 6.9],
+             [0.1, 0.58, 1.06, 1.54, 2.02, 2.5]]
+
+for att in range(4):
+    coefs = coefs_all[att]
+    filtro = np.bitwise_and(data[:, att] >= coefs[0], data[:, att] < coefs[1])
+    data_pert[filtro, att, 0] = calc_y_desc(data[filtro, att], [coefs[0], coefs[1]])
+    for i in range(1, 5):
+        filtro = np.bitwise_and(data[:, att] >= coefs[i-1], data[:, att] < coefs[i])
+        data_pert[filtro, att, i] = calc_y_asc(data[filtro, att], [coefs[i-1], coefs[i]])
+        filtro = np.bitwise_and(data[:, att] >= coefs[i], data[:, att] < coefs[i+1])
+        data_pert[filtro, att, i] = calc_y_desc(data[filtro, att], [coefs[i], coefs[i+1]])
+    i=5
+    filtro = np.bitwise_and(data[:, att] >= coefs[i - 1], data[:, att] < coefs[i])
+    data_pert[filtro, att, i-1] = calc_y_asc(data[filtro, att], [coefs[i - 1], coefs[i]])
+
+for k in range(data.shape[0]):
+    for l in range(data.shape[1]):
+        data_fuzzy[k,l] = NOMES[np.argmax(data_pert[k,l])]
+
+att = 0
+
+filtro = labels == 'Iris-setosa'
+unique, counts = np.unique(data_fuzzy[filtro,att], return_counts=True)
+print( dict(zip(unique, counts)))
+
+
+filtro = labels == 'Iris-versicolor'
+unique, counts = np.unique(data_fuzzy[filtro,att], return_counts=True)
+print( dict(zip(unique, counts)))
+
+
+filtro = labels == 'Iris-virginica'
+unique, counts = np.unique(data_fuzzy[filtro,att], return_counts=True)
+print( dict(zip(unique, counts)))
