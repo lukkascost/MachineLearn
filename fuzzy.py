91f02440abfcca44f4d229a03f6e5a0dfc61d5e6
lukkascost <lucas.costa@lit.ifce.edu.br>
2019-06-28 09:35:28 -0300

    fuzzy implementation

--- fuzzy.py
+++ fuzzy.py
@@ -13,27 +13,29 @@ coefs_all = [[4.3, 5.02, 5.74, 6.46, 7.18, 7.9],
              [0.1, 0.58, 1.06, 1.54, 2.02, 2.5]]
 
 oExp = Experiment()
-for learn in range(0, 1, 1):
-    oDataSet = DataSet()
-    for j, i in enumerate(data):
-        oDataSet.add_sample_of_attribute(np.array(list(i) + [labels[j]]))
-    oDataSet.attributes = oDataSet.attributes.astype(float)
-    oDataSet.labelsNames.append("SEM INFORMACAO")
-    # oDataSet.attributes = np.nan_to_num(
-    #     (oDataSet.attributes - np.mean(oDataSet.attributes, axis=0)) / np.std(oDataSet.attributes, axis=0))
+# for learn in range(0, 1, 1):
+#     oDataSet = DataSet()
+#     for j, i in enumerate(data):
+#         oDataSet.add_sample_of_attribute(np.array(list(i) + [labels[j]]))
+#     oDataSet.attributes = oDataSet.attributes.astype(float)
+#     oDataSet.labelsNames.append("SEM INFORMACAO")
+#     # oDataSet.attributes = np.nan_to_num(
+#     #     (oDataSet.attributes - np.mean(oDataSet.attributes, axis=0)) / np.std(oDataSet.attributes, axis=0))
+#
+#     for iteration in range(50):
+#         oData = Data(4, 10)
+#
+#         oData.random_training_test_by_percent([50, 50, 50], 0.8)
+#         fuzzy = Fuzzy(NOMES, coefs_all)
+#         fuzzy.train(oDataSet.attributes[oData.Training_indexes], oDataSet.labels[oData.Training_indexes])
+#         for x in oData.Testing_indexes:
+#             oData.confusion_matrix[int(fuzzy.predict(fuzzy.fuzzy_data(np.array([oDataSet.attributes[x]]))[0]))][int(oDataSet.labels[x])]+= 1
+#         oData.model = fuzzy
+#         oDataSet.append(oData)
+#     oExp.add_data_set(oDataSet, "Experimento variando Com 10 atributos .".format())
+#     oExp.save("Experiment_03_01_MLP_BK.gzip")
+#     oExp.save("Experiment_03_01_MLP.gzip")
 
-    for iteration in range(50):
-        oData = Data(4, 10)
-
-        oData.random_training_test_by_percent([50, 50, 50], 0.8)
-        fuzzy = Fuzzy(NOMES, coefs_all)
-        fuzzy.train(oDataSet.attributes[oData.Training_indexes], oDataSet.labels[oData.Training_indexes])
-        for x in oData.Testing_indexes:
-            oData.confusion_matrix[int(fuzzy.predict(fuzzy.fuzzy_data(np.array([oDataSet.attributes[x]]))[0]))][int(oDataSet.labels[x])]+= 1
-        oData.model = fuzzy
-        oDataSet.append(oData)
-    oExp.add_data_set(oDataSet, "Experimento variando Com 10 atributos .".format())
-    oExp.save("Experiment_03_01_MLP_BK.gzip")
-    oExp.save("Experiment_03_01_MLP.gzip")
-    print(oExp)
-    print(oDataSet.sum_confusion_matrix/50)
+oExp = Experiment.load("Experiment_03_01_MLP.gzip")
+print(oExp)
+print(oExp.experimentResults[0].sum_confusion_matrix/50)
